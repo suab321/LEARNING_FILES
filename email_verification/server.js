bbb9453966db5bcec0c27c6e05af97c412a722c1
suab321 <adityasaha890@gmail.com>
2018-08-12 17:09:07 +0530

    sc

---
+++ email_verification/server.js
@@ -0,0 +1,78 @@
+const express=require('express')
+const bodyparser=require('body-parser')
+const mongoose=require('mongoose')
+const mailer=require('nodemailer')
+
+
+var transporter=mailer.createTransport({
+	service:'gmail',
+	auth:{
+		user:'adityasaha890@gmail.com',
+		pass:'9051571833'
+	}
+});
+
+
+mongoose.Promise=global.Promise
+mongoose.connect('mongodb://127.0.0.1/testdb')
+var temp_user=new mongoose.Schema({
+	email:{type:String,required:true,unique:true},
+	pass:{type:String}
+})
+var permanant_user=new mongoose.Schema({
+	email:{type:String},
+	pass:{type:String}
+})
+const permanant_model=mongoose.model('permanant_table',permanant_user)
+const temp_model=mongoose.model('temporary_user_table',temp_user)
+const app=express()
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
+
+app.post('/register',(req,res)=>{
+	const temp_table=new temp_model
+	temp_table.email=req.body.email
+	temp_table.pass=req.body.pass
+	temp_table.save((err,user)=>{(err)?console.log(err):send(req.body.email)})
+
+})
+app.get('/verfify/:email',(req,res)=>{
+	const{email}=req.params;
+	temp_model.find({email:email},(err,user)=>{
+		if(err)
+			console.log(err)
+		else{
+			const model=new permanant_model
+			model.email=user[0].email
+			model.pass=user[0].pass
+			model.save((err,user)=>{
+				if(err)
+					res.status(500).json(err)
+				else
+					res.status(200).json(user)
+			})
+
+		}
+	})
+})
+
+
+const send=(email)=>{
+var mailoption={
+	from:'adityasaha890@gmail.com',
+	to:email,
+	subject:'Using nodemailer',
+	text:'From nodemailer express Click the link below for verification',
+	html:'<p>Click for verification<a href="http://localhost:3000/verfify/'+email+'">For verification</a></p>'
+};
+
+transporter.sendMail(mailoption).then((info)=>{
+	console.log(info)
+})
+.catch((err)=>{
+	console.log(err)
+})
+}
+
+app.listen(3000)
--- express-validator/server.js
+++ express-validator/server.js
@@ -15,18 +15,10 @@ app.use(session({
 	resave:'false',
 	saveUninitialized:true
 }))
-app.use((req,res,next)=>{
-	if(!req.session.views)
-		req.session.view={}
-	let path=parseurl(req).pathname
-	req.session.views[path]=(req.session.views[path]||0)+1
-	next();
-})
-
 app.post('/submit',(req,res)=>{
 	req.check('Email','invalid email').isEmail();
 	let email_err=req.validationErrors()
-	req.check('Password','password invalid').isLenght({min:5}).equals(req.body.CPassword)
+	req.check('Password','password invalid').isLength({min:5}).equals(req.body.CPassword)
 	let pass_err=req.validationErrors();
 	if(email_err)
 		res.render('new',{msg:email_err})
