77594a41fdc89bc5ceb2f3ab7cd7bc1c4d989f4e
Abhinav Singh <adityasaha890@gmail.com>
2019-03-28 03:54:28 +0530

    f

--- charity/charity_backend/url.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    database_link:"mongodb://127.0.0.1/charity",
-    frontend_link:"http://localhost:3000"
-}
--- charity/charity_frontend/src/components/url.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    backend:"http://localhost:3002"
-}
