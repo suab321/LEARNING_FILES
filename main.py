af1831d1032bffb0ba2dcd9beb8b5d0d22f094db
Abhinav Singh <adityasaha890@gmail.com>
2019-05-31 22:14:30 +0530

    G

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import subprocess
 GITHUB_USERNAME="suab321"
 GITHUB_PASSWORD="Suab@123"
 
-subprocess.run(['git add .','git commit -m"F"','git push'],shell=True)
+subprocess.run('git add .',shell=True)
+subprocess.run('git add .',shell=True)
+subprocess.run('git push',shell=True)
